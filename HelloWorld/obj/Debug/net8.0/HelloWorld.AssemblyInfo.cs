f2416c69a22e10cc51ecf73e8aeae836c1316961
manlikeclaro <ckibii08@gmail.com>
2024-06-26 11:55:29 +0300

    [fix] removed ide settings from vcs

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183c7530fc0e855501d7f09399f9a6fa9f48ce02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4432faa3c24cc824b32cbb24ec6f38079563c869")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
